5ad34812cd16571baa2bcbee2c043b4cf4e9a49a
Lucasrdh <lucaschretien38@hotmail.fr>
2024-07-29 16:00:09 +0200

    fait les routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/product',function () {
+    return 'Liste des produits';
+});
+
+route::get('/product/{id}',function($id) {
+    return"Fiche du produit $id";
+});
+
+route::get('/cart',function (){
+    return "Panier";
+});
+
